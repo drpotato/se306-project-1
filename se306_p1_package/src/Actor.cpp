4936dfe45c69793dffdabfd69394fac25aadb185
joshofreeness <joshofreeness@gmail.com>
2014-08-07 12:41:12 +1200

    Added empty Actor class and header file

---
+++ se306_p1_package/src/Actor.cpp
@@ -0,0 +1,22 @@
+#include "ros/ros.h"
+#include "std_msgs/String.h"
+#include <geometry_msgs/Twist.h>
+#include <nav_msgs/Odometry.h>
+#include <sensor_msgs/LaserScan.h>
+
+#include <sstream>
+#include "math.h"
+
+#ifndef ACTORMAIN
+#define ACTORMAIN
+
+class Actor
+{
+
+
+
+
+};
+
+
+#endif 
---
+++ se306_p1_package/src/Actor.h
@@ -0,0 +1,20 @@
+#ifndef ACTORMAIN
+#define ACTORMAIN
+
+class Actor
+{
+
+	private;
+	//velocity of the robot
+	double linear_x;
+	double angular_z;
+
+	//pose of the robot
+	double px;
+	double py;
+	double theta;
+};
+
+
+#endif
+
