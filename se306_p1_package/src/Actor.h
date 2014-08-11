5443ef47ded6b6ef19bf228a3cef9d639bbd7413
Kurt McAlpine <kurtmcalpine@gmail.com>
2014-08-11 12:27:17 +1200

    Overriding virtual method in R0 from Actor

--- se306_p1_package/src/Actor.h
+++ se306_p1_package/src/Actor.h
@@ -20,7 +20,7 @@ public:
 	//void StageOdom_callback(nav_msgs::Odometry msg);
 	//void StageLaser_callback(sensor_msgs::LaserScan msg);
 	//int main(int argc, char **argv);
-	//virtual int mainHook(int argc, char **argv);
+	virtual int mainHook(int argc, char **argv) = 0;
 };
 
 
