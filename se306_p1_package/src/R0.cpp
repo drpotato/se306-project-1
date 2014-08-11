4b26386277eb63500ca5ddb2b904981d4ebd4dd9
Jenna Finlayson <jennafinlayson@gmail.com>
2014-08-11 13:59:38 +1200

    Prepared R0 for hook method implementation for location publishing and subscribing

--- se306_p1_package/src/R0.cpp
+++ se306_p1_package/src/R0.cpp
@@ -32,59 +32,22 @@ void StageLaser_callback(sensor_msgs::LaserScan msg)
 {
     //This is the callback function to process laser scan messages
     //you can access the range data from msg.ranges[i]. i = sample number
-    
 }
 
-virtual void init(int argc, char **argv) {
+virtual void initialSetup(int argc, char **argv) {
   //initialize robot parameters
   //Initial pose. This is same as the pose that you used in the world file to set	the robot pose.
   theta = M_PI/2.0;
   px = 10;
   py = 20;
   
-  //Initial velocity
-  linear_x = 0.2;
-  angular_z = 0.2;
-  
   //You must call ros::init() first of all. ros::init() function needs to see argc and argv. The third argument is the name of the node
   ros::init(argc, argv, "RobotNode0");
   
   //NodeHandle is the main access point to communicate with ros.
   ros::NodeHandle n;
-  
-  //advertise() function will tell ROS that you want to publish on a given topic_
-  //to stage
-  ros::Publisher RobotNode_stage_pub = n.advertise<geometry_msgs::Twist>("robot_0/cmd_vel",1000);
-  
-  //subscribe to listen to messages coming from stage
-  ros::Subscriber StageOdo_sub = n.subscribe<nav_msgs::Odometry>("robot_0/odom",1000, StageOdom_callback);
-  ros::Subscriber StageLaser_sub = n.subscribe<sensor_msgs::LaserScan>("robot_0/base_scan",1000,StageLaser_callback);
-  
-  ros::Rate loop_rate(10);
-  
-  //a count of howmany messages we have sent
-  int count = 0;
-  
-  ////messages
-  //velocity of this RobotNode
-  geometry_msgs::Twist RobotNode_cmdvel;
 }
 
-virtual void executeHook() {
-  //messages to stage
-  RobotNode_cmdvel.linear.x = linear_x;
-  RobotNode_cmdvel.angular.z = angular_z;
-
-  //publish the message
-  RobotNode_stage_pub.publish(RobotNode_cmdvel);
-
-  ros::spinOnce();
-
-  loop_rate.sleep();
-  ++count;
+virtual void executeInfiniteLoopHook() {
+  
 }
-
-//virtual int mainHook(int argc, char **argv)
-//{        
-//  return 0;
-//}
