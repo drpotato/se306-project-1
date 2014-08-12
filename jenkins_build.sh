fdd7acb9383a082ea1103486dceb3e1761de9696
Kurt McAlpine <kurtmcalpine@gmail.com>
2014-08-13 08:11:13 +1200

    jenkins_build.sh now creates a tarball with all the files required to run the project

--- jenkins_build.sh
+++ jenkins_build.sh
@@ -6,3 +6,13 @@ sudo rosdep init
 rosdep update
 
 $WORKSPACE/build.sh
+
+# Create a tarball with everything needed to run
+mkdir -p s$WORKSPACE/e306_runnable/src/se306_p1_pkg
+
+cp $WORKSPACE/run.sh $WORKSPACE/se306_runnable/run.sh
+cp -r $WORKSPACE/src/se306_p1_pkg/world/ $WORKSPACE/se306_runnable/src/se306_p1_pkg/world/
+cp -r $WORKSPACE/devel/ $WORKSPACE/se306_runnable/devel/
+
+tar -cvf $WORKSPACE/se306_runnable.tar $WORKSPACE/se306_runnable
+rm -rf $WORKSPACE/se306_runnable
