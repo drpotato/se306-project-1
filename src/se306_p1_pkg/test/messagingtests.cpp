9e1bfd02ed6986d7aa59e63af74bb1316c2c9318
Michael Lo <nz.ampersand@gmail.com>
2014-08-14 22:54:17 +1200

    Commented out my tests as they do not compile and I don't want to push/commit nonworking builds. Code is still there though and can be fixed later.

--- src/se306_p1_pkg/test/messagingtests.cpp
+++ src/se306_p1_pkg/test/messagingtests.cpp
@@ -1,3 +1,5 @@
+/*
+
 // Bring in my package's API, which is what I'm testing
 #include "../src/Resident.h"
 #include "../src//ActorSpawner.h"
@@ -29,3 +31,5 @@ int main(int argc, char **argv){
 testing::InitGoogleTest(&argc, argv);
 return RUN_ALL_TESTS();
 }
+
+*/
