4e17c75ed7ac7a47b8e7cbe4b5bea02a5049c116
Jenna Finlayson <jennafinlayson@gmail.com>
2014-08-15 12:06:02 +1200

    fixed really bad brackets

--- src/se306_p1_pkg/src/EntertainmentRobot.cpp
+++ src/se306_p1_pkg/src/EntertainmentRobot.cpp
@@ -34,7 +34,9 @@ void EntertainmentRobot::doExecuteLoop()
 		if (checkEntertainmentLevel())
 		{
 			//ROS_INFO("Nothing to do here");
-		} else {
+		} 
+		else 
+		{
 			if (first_call)
 			{
 				this->activeNode = &node5;
@@ -48,7 +50,8 @@ void EntertainmentRobot::doExecuteLoop()
 
 
 	    	//The or in this case is just for the alpha, remove once the robot is capable of reaching the resident
-	    	if (!(this->movingToResident) ){
+	    	if (!(this->movingToResident) )
+	    	{
 	    		//EntertainmentRobot::doResponse("entertaining");
 	    		ROS_INFO("CHANGED TO ENTERTAINING");
 	    		entertaining=true;
@@ -58,7 +61,9 @@ void EntertainmentRobot::doExecuteLoop()
 			//After finished entertaining set entertaining to flase
 
 		}
-	} else {
+	} 
+	else 
+	{
 		if (entertainednessLevel == 5)
 		{
 			//Add do last desponse call that kurt implimented
@@ -66,13 +71,16 @@ void EntertainmentRobot::doExecuteLoop()
 			entertaining = false;
 
 
-		} else
+		} 
+		else
 		{
 
-			if (y == 40){
+			if (y == 40)
+			{
 				EntertainmentRobot::doResponse("entertaining");
 				y=0;
-			} else 
+			} 
+			else 
 			{
 				y++;
 			}
@@ -96,7 +104,8 @@ void EntertainmentRobot::entertainednessCallback(msg_pkg::Entertainedness msg)
 
 bool EntertainmentRobot::checkEntertainmentLevel()
 {
-	if (entertainednessLevel>=2 ){
+	if (entertainednessLevel>=2 )
+	{
 		return true;
 	}
 	return false;
