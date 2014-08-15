934ecc4a11f673fae0990cd0685d91490081ab6f
Kurt McAlpine <kurtmcalpine@gmail.com>
2014-08-15 14:09:22 +1200

    Spelling mistake in demo.sh

--- demo.sh
+++ demo.sh
@@ -4,4 +4,4 @@ nohup ./run.sh &
 sleep 3s
 $TERM -hold -title "socialness" -e $SHELL -c "source devel/setup.bash; rostopic echo socialness" &
 $TERM -hold -title "entertainedness" -e $SHELL -c "source devel/setup.bash; rostopic echo entertainedness" &
-$TERM -hold -title "interation" -e $SHELL -c "source devel/setup.bash; rostopic echo interaction" &
+$TERM -hold -title "interaction" -e $SHELL -c "source devel/setup.bash; rostopic echo interaction" &
