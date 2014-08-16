d5717c207a4df9ba08c100356aafdac2536ebd5a
Jamie Peterson <jpet987@aucklanduni.ac.nz>
2014-08-17 01:51:58 +1200

    debug.sh is now generated by the build process, and the generation can be configured with debugconfig. Produces logs in a "logs" subfolder by default

--- build.sh
+++ build.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 export ROS_PACKAGE_PATH=`pwd`:$ROS_PACKAGE_PATH
 
-python build_includeActors.py
+python build_updateBuild.py
 catkin_make
 if [ $? -ne 0 ]; then
  echo "The build failed"
  exit 1
 fi
 
-python build_updateActors.py
+python build_updateRun.py
 
 chmod 755 ./run.sh
---
+++ persist.sh
@@ -0,0 +1,6 @@
+$1
+until [[ $? -eq 0 ]]
+do sleep 0.1s
+$1
+done
+echo uhh
