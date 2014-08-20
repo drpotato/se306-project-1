b1155346df643ed2058a65e76d2d47c494d5ca66
joshofreeness <joshofreeness@gmail.com>
2014-08-21 10:12:02 +1200

    Added subscriber to Health for Doctor

--- src/se306_p1_pkg/src/Doctor.cpp
+++ src/se306_p1_pkg/src/Doctor.cpp
@@ -7,11 +7,30 @@ void Doctor::doInitialSetup()
 {
 	velLinear = 0.0;
     velRotational = 0.0;
+    subscriberHealth = nodeHandle->subscribe("socialness", 1000, Doctor::healthCallback);
 }
 
 
 
 void Doctor::doExecuteLoop()
 {
+
+}
+
+void Doctor::healthCallback(msg_pkg::Health msg)
+{
+
+	Doctor* temp = dynamic_cast<Doctor*>( ActorSpawner::getInstance().getActor());
+	temp->healthLevel = msg.level;
+
+	if (msg.level < 1){
+		temp->emergency();
+	}
+
+}
+
+void Doctor::emergency()
+{
+	ROS_DEBUG("HEARD THAT THERE IS AN EMERGENCY");
 
 }
--- src/se306_p1_pkg/src/Doctor.h
+++ src/se306_p1_pkg/src/Doctor.h
@@ -2,6 +2,7 @@
 #define SE306P1_ACTOR_DOCTOR_H_DEFINED
 
 #include "Visitor.h"
+#include <msg_pkg/Health.h>
 #include "ActorSpawner.h"
 
 class Doctor : public Visitor
@@ -9,6 +10,13 @@ class Doctor : public Visitor
 protected:
   virtual void doInitialSetup();
   virtual void doExecuteLoop();
+  void emergency();
+
+  static void healthCallback(msg_pkg::Health msg);
+
+  ros::Subscriber subscriberHealth;
+
+  int8_t healthLevel;
 
 
 };
