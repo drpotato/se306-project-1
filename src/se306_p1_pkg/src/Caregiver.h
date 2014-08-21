ccfb6934a9d0b2c4802392c05df79c02375c17a6
Chaoyi Qin <qwyqqcy@users.noreply.github.com>
2014-08-21 16:03:13 +1200

    Update Caregiver.h

--- src/se306_p1_pkg/src/Caregiver.h
+++ src/se306_p1_pkg/src/Caregiver.h
@@ -3,12 +3,20 @@
 
 #include "Visitor.h"
 #include "ActorSpawner.h"
+#include <msg_pkg/Fitness.h>
+#include <msg_pkg/Hunger.h>
+#include <msg_pkg/Hygiene.h>
+#include <msg_pkg/Morale.h>
+#include <msg_pkg/Socialness.h>
 
 class Caregiver : public Visitor
 {
 protected:
     virtual void doInitialSetup();
     Virtual void doExecuteLoop();
+    
+    
+    ros::Subscriber subscriberFitness;
 };
 
 
