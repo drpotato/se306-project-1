3d1b07408e3b7f262e22b2f3dbceb911b2dc1e38
Chaoyi Qin <qwyqqcy@users.noreply.github.com>
2014-08-21 14:43:37 +1200

    Create Caregiver.cpp

---
+++ src/se306_p1_pkg/src/Caregiver.cpp
@@ -0,0 +1,14 @@
+#include "Caregiver.h"
+
+#include "PathPlanner.h"
+#include "PathPlannerNode.h"
+
+void Caregiver::doInitialSetup()
+{
+    velLinear = 0.0;
+    velRotational = 0.0;
+}
+
+void Caregiver::doExecuteLoop()
+{
+}
