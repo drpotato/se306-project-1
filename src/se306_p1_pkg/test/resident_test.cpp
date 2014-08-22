11e3d04b4903fe960f053f0655e8f439b00ee696
Kurt McAlpine <kurtmcalpine@gmail.com>
2014-08-22 13:33:28 +1200

    Added a test to test getNewLevel in resident

--- src/se306_p1_pkg/test/resident_test.cpp
+++ src/se306_p1_pkg/test/resident_test.cpp
@@ -17,6 +17,26 @@ ASSERT_EQ(resident->morale_count_, 0);
 ASSERT_EQ(resident->socialness_count_, 0);
 }
 
+TEST(TestSuite, test_resident_get_new_level)
+{
+Resident *resident = new Resident();
+int result = resident->getNewLevel(0, 0);
+ASSERT_EQ(result, 1);
+result = resident->getNewLevel(0, 1);
+ASSERT_EQ(result, 1);
+result = resident->getNewLevel(0, 2);
+ASSERT_EQ(result, 2);
+result = resident->getNewLevel(0, 3);
+ASSERT_EQ(result, 3);
+result = resident->getNewLevel(0, 4);
+ASSERT_EQ(result, 4);
+result = resident->getNewLevel(0, 5);
+ASSERT_EQ(result, 5);
+result = resident->getNewLevel(0, 6);
+ASSERT_EQ(result, 5);
+}
+
+
 TEST(TestSuite, test_interaction_msg)
 {
 msg_pkg::Interaction *msg = new msg_pkg::Interaction();
