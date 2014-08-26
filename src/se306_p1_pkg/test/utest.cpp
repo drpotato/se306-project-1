62e5b8d5cd2369daadae10a9490b026bce8a5c75
Kurt McAlpine <kurtmcalpine@gmail.com>
2014-08-26 13:31:07 +1200

    Tests using callbacks actually work

--- src/se306_p1_pkg/test/utest.cpp
+++ src/se306_p1_pkg/test/utest.cpp
@@ -1,7 +1,45 @@
-// Bring in my package's API, which is what I'm testing
-#include "../src/Actor.h"
 // Bring in gtest
 #include <gtest/gtest.h>
+
+// Bring in my package's API, which is what I'm testing
+#include "../src/Resident.h"
+#include "../src/ActorSpawner.h"
+
+
+namespace {
+
+// The fixture for testing class Foo.
+class FooTest : public ::testing::Test {
+ protected:
+  // You can remove any or all of the following functions if its body
+  // is empty.
+
+  FooTest() {
+    // You can do set-up work for each test here.
+  }
+
+  virtual ~FooTest() {
+    // You can do clean-up work that doesn't throw exceptions here.
+  }
+
+  // If the constructor and destructor are not enough for setting up
+  // and cleaning up each test, you can define the following methods:
+
+  virtual void SetUp() {
+    // Code here will be called immediately after the constructor (right
+    // before each test).
+    system("source devel/setup.bash; roscore &");
+  }
+
+  virtual void TearDown() {
+    // Code here will be called immediately after each test (right
+    // before the destructor).
+    system("killall roscore");
+  }
+
+  // Objects declared here can be used by all tests in the test case for Foo.
+};
+}
 
 // Declare a test
 TEST(TestSuite, testCase1)
@@ -13,6 +51,18 @@ ASSERT_TRUE(1);
 TEST(TestSuite, testCase2)
 {
 ASSERT_TRUE(1);
+}
+
+TEST(TestSuite, testCase3)
+{
+  ActorSpawner &spawner = ActorSpawner::getInstance();
+  Resident *actor = (Resident*)spawner.getActor("Resident");
+  
+  msg_pkg::Interaction *msg = new msg_pkg::Interaction();
+  actor->interactionCallback(*msg);
+
+  actor->initialSetup(0, -5, 3, 0);
+  actor->executeLoop();
 }
 
 // Run all the tests that were declared with TEST()
